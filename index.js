cbb6edcabf204f208fdaedb70f7d0fb499cf8333
Zxey10 <68563478+Zxey10@users.noreply.github.com>
2021-12-21 22:16:53 +0200

    test extension

---
+++ index.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('#checkPage');
+
+
+btn.addEventListener('click',() => {
+    // chrome.tabs.getSelected(null,function(tab){
+        
+    // })
+    alert(1)
+})
