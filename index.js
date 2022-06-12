c4cff32f197d64829e3c075fee2557cda719a367
Zxey10 <68563478+Zxey10@users.noreply.github.com>
2022-06-12 21:08:48 +0300

    SITE BLOCKER

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const btn = document.querySelector('#checkPage');
 
-
-btn.addEventListener('click',() => {
-    // chrome.tabs.getSelected(null,function(tab){
-        
-    // })
-    alert(1)
+let redirectUrl = ''
+let blockedUrl = ''
+fetch("links.json")
+.then((response) => {
+      return response.json();
 })
+.then((json) => {
+      console.log(json);
+      redirectUrl = json.REDIRECT_URL
+      blockedUrl = json.BLOCKED_URL
+});
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+        console.log('Page Loaded')
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            let url = tabs[0].url;
+            // use `url` here inside the callback because it's asynchronous!
+            console.log(url)
+            if(url.includes(blockedUrl)){
+                console.log('Redirect ...')
+                chrome.tabs.update({url: redirectUrl});
+                window.close()
+            }
+        });
+    }
+  })
+
+
+function randomColor(){
+    let r = Math.floor(Math.random()*255)
+    let g = Math.floor(Math.random()*255)
+    let b = Math.floor(Math.random()*255)
+    return `rgb(${r},${g},${b})`;
+}
